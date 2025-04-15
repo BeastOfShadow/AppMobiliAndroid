5225d42945962bb75c94d8f45cc08123ea73a3df
BeastOfShadow <simone.negro.2002@gmail.com>
2025-04-15 21:38:18 +0200

    Import storage

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,6 +70,7 @@ dependencies {
     implementation(libs.androidx.material3.lint)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.coil.compose.v240)
+    implementation(libs.firebase.storage.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
