b6d3b0d4f7c6cd5bbe8187237142b9b9a7d85f7d
BeastOfShadow <simone.negro.2002@gmail.com>
2025-05-30 09:40:09 +0200

    Filtered tasks

--- app/src/main/java/it/uniupo/ktt/ui/pages/TaskManagerScreen.kt
+++ app/src/main/java/it/uniupo/ktt/ui/pages/TaskManagerScreen.kt
@@ -251,26 +251,18 @@ fun TaskManagerScreen(navController: NavController) {
 
                                                         val uid = currentUid()
                                                         if (uid != null) {
-                                                            readyTasks =
-                                                                taskViewModel.getTasksByStatus(
-                                                                    uid,
-                                                                    TaskStatus.READY.toString()
-                                                                )
-                                                            ongoingTasks =
-                                                                taskViewModel.getTasksByStatus(
-                                                                    uid,
-                                                                    TaskStatus.ONGOING.toString()
-                                                                )
-                                                            completedTasks =
-                                                                taskViewModel.getTasksByStatus(
-                                                                    uid,
-                                                                    TaskStatus.COMPLETED.toString()
-                                                                )
-                                                            ratedTasks =
-                                                                taskViewModel.getTasksByStatus(
-                                                                    uid,
-                                                                    TaskStatus.RATED.toString()
-                                                                )
+                                                            readyTasks = taskViewModel
+                                                                .getTasksByStatus(uid, TaskStatus.READY.toString())
+                                                                .filter { isToday(it.createdAt) }
+                                                            ongoingTasks = taskViewModel
+                                                                .getTasksByStatus(uid, TaskStatus.ONGOING.toString())
+                                                                .filter { isToday(it.createdAt) }
+                                                            completedTasks = taskViewModel
+                                                                .getTasksByStatus(uid, TaskStatus.COMPLETED.toString())
+                                                                .filter { isToday(it.createdAt) }
+                                                            ratedTasks = taskViewModel
+                                                                .getTasksByStatus(uid, TaskStatus.RATED.toString())
+                                                                .filter { isToday(it.createdAt) }
                                                         }
                                                     }
                                                 }
